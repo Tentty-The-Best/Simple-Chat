2c9aa22badf5e90ba3396f90ae77044a448dc11c
Tentty-The-Best <tentquickspeed28@gmail.com>
2025-11-16 15:39:02 -0500

    Update server.js
    yum

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*"
+  }
+});
 
 app.use(express.static("public"));
 
